293269c9a7f125dfa1c42c2011c17c3301e1abb1
Alper <alpr35@windowslive.com>
2015-04-07 17:23:49 +0200

    Lab

---
+++ CD.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace uygulama
+{
+    class CD:Interface1 
+    {
+        string isim;
+
+        public string Isim
+        {
+            get { return isim; }
+            set { isim = value; }
+        }
+        string turu;
+        string ozet;
+        public CD(string isim, string turu, string ozet)
+        {
+            this.isim = isim;
+            this.turu = turu;
+            this.ozet = ozet;
+        }
+
+        public string gözat()
+        {
+           oduncTarihi=
+        }
+
+    }
+}
---
+++ kitap.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace uygulama
+{
+   abstract class kitap:IoduncAlınabilir,Interface1 
+    {
+        string yazar;
+        string ISBN;
+        string isim;
+
+        public string Isim
+        {
+            get { return isim; }
+            set { isim = value; }
+        }
+        DateTime odunctarihi;
+       string ozet;
+        bool oduncalındı;
+        public kitap(string yazar,string ISBN,string isim,DateTime odunctarihi,string ozet,bool oduncalındı)
+        {
+            this.yazar = yazar;
+            this.ISBN = ISBN;
+            this.isim =isim ;
+            this.odunctarihi = odunctarihi;
+            this.oduncalındı = oduncalındı;
+            this .ozet =ozet =
+        }
+
+       
+       public virtual string ToSitring()
+       {
+       return "isim"+this .isim +"\nyazar"+this .yazar ;
+       }
+
+       public void oduncal(DateTime Date)
+       {
+           odunctarihi =Date;
+           oduncalındı =true ;
+       }
+
+       public string  Gözat()
+       {
+           return ozet ;
+       }
+
+
+       public void Iadeet()
+       {
+       oduncalındı =false ;
+       }
+    }
+}
